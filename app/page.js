512acc106713cf4d6f993e975e43aee3d6df77ed
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-06 11:14:10 +0600

    feat: add metadata for Home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import { Fragment } from "react";
 
+const metadata = {
+  title: "DocuCraft",
+  description: "DocuCraft - A documentation website",
+};
+
 export default function Home() {
   return (
     <Fragment>
